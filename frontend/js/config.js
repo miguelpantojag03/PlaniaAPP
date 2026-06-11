3cdadd3c1b060e98efadd5d6f381660cd52a8ff6
miguelpantojag03 <miguelpantojag03@github.com>
2026-06-11 10:11:22 -0500

    Point frontend to production backend

--- frontend/js/config.js
+++ frontend/js/config.js
@@ -1,1 +1,4 @@
-window.PLANIA_API_BASE_URL = window.PLANIA_API_BASE_URL || "http://localhost:8080/api";
+const isLocalPlaniaHost = ["localhost", "127.0.0.1"].includes(window.location.hostname);
+
+window.PLANIA_API_BASE_URL = window.PLANIA_API_BASE_URL
+    || (isLocalPlaniaHost ? "http://localhost:8080/api" : "https://plania-backend.onrender.com/api");
